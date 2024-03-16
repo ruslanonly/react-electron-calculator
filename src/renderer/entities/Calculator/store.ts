1514ee7344de84a81034700fe9e6d5f6e80cd169
Vsevolod Fadeev <sevafad@gmail.com>
2024-03-16 18:31:26 +0300

    Add setScreen action.

--- src/renderer/entities/Calculator/store.ts
+++ src/renderer/entities/Calculator/store.ts
@@ -1,9 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+
+interface ICalculatorState {
+  screen: string;
+  operation: string;
+  result: number;
+}
+
+const initialState: ICalculatorState = {
+  operation: '',
+  screen: '',
+  result: 0,
+};
+
+const calculatorSlice = createSlice({
+  name: 'calculator',
+  initialState,
+  reducers: {
+    setScreen: (state, actions: PayloadAction<string>) => {
+      state.screen = state.screen.concat(actions.payload);
+    },
+  },
+});
+
+export const { setScreen } = calculatorSlice.actions;
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    calculator: calculatorSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
