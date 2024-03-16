0ebfd3b4ecbad3abe4f1d4f48b134b5ac3469559
Vsevolod Fadeev <sevafad@gmail.com>
2024-03-16 18:40:12 +0300

    Add useAppSelector hook to shared level.

--- src/renderer/shared/index.ts
+++ src/renderer/shared/index.ts
@@ -1,3 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
