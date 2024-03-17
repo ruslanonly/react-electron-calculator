841b71796a2b50f5bd007d8718fd4d84f1ab6cbe
ruslanonly <rzaevruslanonly@gmail.com>
2024-03-17 12:03:22 +0300

    Add Main UI

--- src/renderer/widgets/Calculator/ui/CalculatorWidget.tsx
+++ src/renderer/widgets/Calculator/ui/CalculatorWidget.tsx
@@ -1,5 +1,12 @@
+import { Screen } from 'features/Calculator/Screen';
+import { Controls } from 'features/Calculator/Controls';
 import './CalculatorWidget.css';
 
 export function CalculatorWidget() {
-  return <div className="CalculatorWidget">Calculator</div>;
+  return (
+    <div className="CalculatorWidget">
+      <Screen />
+      <Controls />
+    </div>
+  );
 }
