42c57aea7cd7d9dee659dcd21bfd6ae760f4897f
Vsevolod Fadeev <sevafad@gmail.com>
2024-03-27 20:39:29 +0300

    Create executeBinaryOperation base.

--- src/renderer/entities/Calculator/lib/useCalculator.ts
+++ src/renderer/entities/Calculator/lib/useCalculator.ts
@@ -1,16 +1,29 @@
-import { setScreen } from 'entities/Calculator';
 import { useAppDispatch, useAppSelector } from 'shared';
-import { EOperation, setOperation, setResult } from '../slice';
+import {
+  EOperation,
+  setIsWaiting,
+  setOperation,
+  setResult,
+  setScreen,
+} from '../slice';
 
 export function useCalculator() {
   const dispatch = useAppDispatch();
   const screen = useAppSelector((state) => state.calculator.screen);
+  const result = useAppSelector((state) => state.calculator.result);
+  const binaryOperation = useAppSelector((state) => state.calculator.operation);
+  const isWaiting = useAppSelector((state) => state.calculator.isWaiting);
 
   const clearScreen = () => {
     dispatch(setScreen(''));
   };
 
   const appendDigit = (digit: string) => {
+    if (isWaiting) {
+      dispatch(setScreen(digit));
+      dispatch(setIsWaiting(false));
+      return;
+    }
     const updatedScreen = screen.concat(digit);
     dispatch(setScreen(updatedScreen));
   };
@@ -26,26 +39,45 @@ export function useCalculator() {
   const binaryOperationReducer = (
     x: number,
     y: number,
-    operation: EOperation,
+    operation: EOperation | null,
   ) => {
     switch (operation) {
       case EOperation.Addition:
-        return x + y;
+        return (isNaN(x) ? 0 : x) + y;
       case EOperation.Subtraction:
-        return x - y;
+        return (isNaN(x) ? 0 : x) - y;
       case EOperation.Multiplication:
-        return x * y;
+        return (isNaN(x) ? 1 : x) * y;
       case EOperation.Division:
-        return x / y;
+        return (isNaN(x) ? 1 : x) / y;
+      case null:
+        return y;
       default:
         ((never: never) => {})(operation);
         return NaN;
     }
   };
 
+  const executeBinaryOperation = (next?: EOperation) => {
+    const value = Number(screen);
+    if (isNaN(value)) {
+      return;
+    }
+    const operationResult = binaryOperationReducer(
+      result,
+      value,
+      binaryOperation,
+    );
+    dispatch(setResult(operationResult));
+    dispatch(setScreen(operationResult.toString()));
+    dispatch(setOperation(next ?? null));
+    dispatch(setIsWaiting(true));
+  };
+
   return {
     appendDigit,
     addComma,
     clearScreen,
+    executeBinaryOperation,
   };
 }
--- src/renderer/entities/Calculator/slice.ts
+++ src/renderer/entities/Calculator/slice.ts
@@ -10,12 +10,14 @@ interface ICalculatorState {
   screen: string;
   operation: EOperation | null;
   result: number;
+  isWaiting: boolean;
 }
 
 const initialState: ICalculatorState = {
   operation: null,
   screen: '',
   result: NaN,
+  isWaiting: false,
 };
 
 const calculatorSlice = createSlice({
@@ -26,16 +28,20 @@ const calculatorSlice = createSlice({
       state.screen = action.payload;
     },
 
-    setOperation: (state, action: PayloadAction<EOperation>) => {
+    setOperation: (state, action: PayloadAction<EOperation | null>) => {
       state.operation = action.payload;
     },
 
     setResult: (state, action: PayloadAction<number>) => {
       state.result = action.payload;
     },
+    setIsWaiting: (state, action: PayloadAction<boolean>) => {
+      state.isWaiting = action.payload;
+    },
   },
 });
 
-export const { setScreen, setOperation, setResult } = calculatorSlice.actions;
+export const { setScreen, setOperation, setResult, setIsWaiting } =
+  calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
